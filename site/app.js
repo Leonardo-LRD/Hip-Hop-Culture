7627abd4c7da202eed8c62d13f9ed5cb7f03ce8f
Leonardo-LRD <leonardo.vasconcellos@sptech.school>
2022-07-07 16:48:36 -0300

    Alterando Porta

--- site/app.js
+++ site/app.js
@@ -4,7 +4,8 @@ process.env.AMBIENTE_PROCESSO = "producao";
 var express = require("express");
 var cors = require("cors");
 var path = require("path");
-var PORTA = process.env.AMBIENTE_PROCESSO == "desenvolvimento" ? 3333 : 8080;
+// var PORTA = process.env.AMBIENTE_PROCESSO == "desenvolvimento" ? 3333 : 8080;
+var PORTA = process.env.PORT || 8080;
 
 var app = express();
 
